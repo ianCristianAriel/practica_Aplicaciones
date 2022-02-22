e4c3e26733d8c42102feddc1c5bcecc83ac116c0
Ian Cristian Ariel <ian.00@hotmail.com>
2022-02-22 15:00:34 -0300

    Update mastermind_V2_5.py

--- mastermind_V2_5.py
+++ mastermind_V2_5.py
@@ -14,7 +14,7 @@ codigoValido=False#Estado por defecto que luego se husara para determinar si el 
 """ ---------------------------FUNCIONES---------------------------- """
 
 def generaCodigoPC():
-    """ Se forma el codigo a adivinar y se devuelve mediante return """
+    #Se forma el codigo a adivinar y se devuelve mediante return
     global Tletras, largoCodigo
     i=1
     codigoPC=''
@@ -25,7 +25,7 @@ def generaCodigoPC():
 
 
 def chequeoCodigoUsuario(codigoUsuario):
-    """ Se comprueba que el codigo tenga el largo de letras y letras adecuadas """
+    #Se comprueba que el codigo tenga el largo de letras y letras adecuadas
     global codigoValido
     codigoValido=False
     for c in codigoUsuario:
@@ -40,7 +40,7 @@ def chequeoCodigoUsuario(codigoUsuario):
 
 def puntage(codigoUsuario):
 
-    """ Se calcula cantidad de aciertos del jugador """
+    #Se calcula cantidad de aciertos del jugador
     global buenos, regulares, codigoPC 
     evaluadasBuenas=[False,False,False,False]
     buenos=0
@@ -48,14 +48,14 @@ def puntage(codigoUsuario):
     j1=0
     j2=0
 
-    """ Se recorren los dos codigos al mismo tiempo para encontrar las coincidencias con el mismo indice (buenos) """
+    #Se recorren los dos codigos al mismo tiempo para encontrar las coincidencias con el mismo indice (buenos)
     while (j1<largoCodigo):
         if (codigoUsuario[j1]==codigoPC[j1]):
             buenos+=1
             evaluadasBuenas[j1]=True
         j1+=1
 
-    """ Se recorren los dos codigos al mismo tiempo para encontrar las coincidencias con distinto indice (regulares) """
+    #Se recorren los dos codigos al mismo tiempo para encontrar las coincidencias con distinto indice (regulares)
     for c1 in codigoPC:
         for c2 in codigoUsuario:
             if (c2==c1) and not evaluadasBuenas[j2]:
@@ -64,7 +64,7 @@ def puntage(codigoUsuario):
 
 
 def corroboraGano():
-    """ Se comprueba si la cantidad de buenos es igual al largo del codigo y se devuelve True en caso afirmativo o False en caso negativo """
+    #Se comprueba si la cantidad de buenos es igual al largo del codigo y se devuelve True en caso afirmativo o False en caso negativo
     global buenos, perdio, largoCodigo
     if (buenos==largoCodigo):
         perdio=False
@@ -79,12 +79,12 @@ while intento <= maxIntentos:
 
     codigoUsuario=input('Ingresa un codigo >> ')
 
-    """ Se envia el codigo a la funcion encargada de validar o no el codigo. En caso negativo se muestra mensaje de error y se pide que se reingrese """
+    #Se envia el codigo a la funcion encargada de validar o no el codigo. En caso negativo se muestra mensaje de error y se pide que se reingrese
     chequeoCodigoUsuario(codigoUsuario) #Corregir no funciona, da fuera del rango en evaludads porque toma codigos de mas de 4 digitos
     while not codigoValido:
         chequeoCodigoUsuario(input('Codigo inadecuado. Ingresa un codigo de {} letras entre la {} y la {} >> '.format(largoCodigo, Tletras[0], Tletras[-1])))
 
-    """ Se envia el codigo del usuario a la funcion encargada de establecer el puntage """
+    #Se envia el codigo del usuario a la funcion encargada de establecer el puntage
     puntage(codigoUsuario)
     print('Buenos: {}, Regulares: {}'.format(buenos, regulares))
 
